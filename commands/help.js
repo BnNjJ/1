1821fa2ce60617891baff3cdf502a863b1031263
BnNjJ <45629005+BnNjJ@users.noreply.github.com>
2018-12-31 15:48:14 +0800

    Add files via upload

---
+++ commands/help.js
@@ -0,0 +1,21 @@
+const Discord = require("discord.js");
+const config = require("../config.json");
+let colour = config.colour;
+let footer = config.footer;
+
+module.exports.run = async (client, message, args) => {
+  message.delete();
+
+  let embed = new Discord.RichEmbed()
+  .setTitle("Ticket Bot - Help")
+  .setDescription("**Created by**: CreeperCry04#2755\n\n**new** - Creates new ticket channel.\n**close** - Closes current ticket channel.\n**add (@member)** - Adds member to ticket.\n**remove (@member)** - Removes member from ticket")
+  .setColor(colour)
+  .setTimestamp()
+  .setFooter(footer);
+
+  message.channel.send(embed);
+
+}
+module.exports.help = {
+  name: "help"
+}
---
+++ commands/new.js
@@ -0,0 +1,58 @@
+const Discord = require ("discord.js");
+const config = require("../config.json");
+const colour = config.colour;
+const categoryID = config.CategoryID;
+
+module.exports.run = async (bot, message, args) => {
+  message.delete();
+
+  const embed = new Discord.RichEmbed()
+  .setColor(colour)
+  .setDescription(`:x: You already have a ticket open! If this is incorrect, please contact a staff member immediately!`)
+  .setTimestamp()
+  .setFooter(`${config.footer}`);
+
+  const embed3 = new Discord.RichEmbed()
+  .setColor(colour)
+  .setDescription("Thank you for reaching our support team! A member of our support team will be with you soon!")
+  .setTimestamp()
+  .setFooter(`${config.footer}`);
+
+  let user1 = message.author.username.toLowerCase()
+  if (message.guild.channels.exists(`name`, `ticket-${user1}-${message.author.discriminator}`)) {
+    message.channel.send(embed)
+    return
+  }
+
+  message.guild.createChannel(`ticket-${message.author.username}-${message.author.discriminator}`).then(async c => {
+    c.setParent(categoryID);
+    let supportrole = message.guild.roles.find(`name`, `Support Team`)
+    if(!supportrole) return message.channel.send("The support role doesn't exist! Please create a role called **Support Team**");
+    c.overwritePermissions(message.guild.defaultRole, {
+      VIEW_CHANNEL: false,
+      SEND_MESSAGES: false
+    })
+    c.overwritePermissions(message.member, {
+      VIEW_CHANNEL: true,
+      SEND_MESSAGES: true
+    })
+    c.overwritePermissions(supportrole, {
+      VIEW_CHANNEL: true,
+      SEND_MESSAGES: true
+    })
+
+    const embed2 = new Discord.RichEmbed()
+    .setColor(colour)
+    .setDescription(`Your ticket has been created! Visit your ticket at ${c}`)
+    .setTimestamp()
+    .setFooter(`${config.footer}`);
+
+    message.channel.send(embed2)
+
+    c.send(embed3)
+  })
+
+}
+module.exports.help = {
+	name: "new"
+}
