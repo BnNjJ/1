40c5952310fa0547a4e1614653f9d93b7e2c514a
BnNjJ <45629005+BnNjJ@users.noreply.github.com>
2019-01-05 22:56:16 +0800

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -7,7 +7,7 @@ module.exports.run = async (client, message, args) => {
   message.delete();
 
   let embed = new Discord.RichEmbed()
-  .setTitle("Ticket Bot - Help")
+  .setTitle("HammerLock | Ticket - Help")
   .setDescription("**Created by**: Flyz#0007\n**Donate(Btc)**: 15NcQk3qPrUhoU4hLZR3W52eCguQZb2Jbx\n**new** - Creates new ticket channel.\n**close** - Closes current ticket channel.\n**add (@member)** - Adds member to ticket.\n**remove (@member)** - Removes member from ticket")
   .setColor(colour)
   .setTimestamp()
