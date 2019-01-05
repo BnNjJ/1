08c14e13595c7d65bc8ab46a17ddfb36484eb0ef
BnNjJ <45629005+BnNjJ@users.noreply.github.com>
2019-01-05 23:02:23 +0800

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -8,7 +8,7 @@ module.exports.run = async (client, message, args) => {
 
   let embed = new Discord.RichEmbed()
   .setTitle("HammerLock | Ticket - Help")
-  .setDescription("**Created by**: Flyz#0007\n**Donate(Btc)**: 15NcQk3qPrUhoU4hLZR3W52eCguQZb2Jbx\n**new** - Creates new ticket channel.\n**close** - Closes current ticket channel.\n**add (@member)** - Adds member to ticket.\n**remove (@member)** - Removes member from ticket\n**invite** - Invite HammerLock | Ticket to your server.")
+  .setDescription("**Created by**: Flyz#0007\n\n**Donate(Btc)**: 15NcQk3qPrUhoU4hLZR3W52eCguQZb2Jbx\n\n**new** - Creates new ticket channel.\n**close** - Closes current ticket channel.\n**add (@member)** - Adds member to ticket.\n**remove (@member)** - Removes member from ticket\n**invite** - Invite HammerLock | Ticket to your server.")
   .setColor(colour)
   .setTimestamp()
   .setFooter(footer);
